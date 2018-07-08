c9de4a5e00ed10e52ae6bbf10493c1c4a8878103
RenanZabeu <renanzabeu@yahoo.it>
2018-07-08 20:38:14 +0100

    heroku2

--- settings/staging.py
+++ settings/staging.py
@@ -1,12 +1,15 @@
 from base import *
-import dj_database_url
+
 
 DEBUG = False
 
 # Database
 
 DATABASES = {
-    'default': dj_database_url.config('CLEARDB_DATABASE_URL')
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 # Stripe environment variables
